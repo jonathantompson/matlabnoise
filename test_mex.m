a316162ed6e92d53c08a3cb32b03cdca9a0b9e38
Kristofer Schlachter <kristofe72@gmail.com>
2015-04-13 17:24:00 -0400

    removed ndgrid_normalized.

--- test_mex.m
+++ test_mex.m
@@ -6,7 +6,13 @@ ydim = 512;
 xdim = 511;
 
 % 2D Grid
-[ X, Y, xstep, ystep ] = ndgrid_normalized( xdim, ydim );
+[v, u] = ndgrid(1:ydim, 1:xdim);
+xstep = 1 / (0.5 * (xdim - 1));
+ystep = 1 / (0.5 * (ydim - 1));
+X = (u - 1) .* xstep - 1;  % [-1, 1]
+Y = (v - 1) .* ystep - 1;  % [-1, 1]
+
+% [ X, Y, xstep, ystep ] = ndgrid_normalized( xdim, ydim );
 
 %HERMITE NOISE
 figure;
