614826e5393ff0f3c57f5a1a3783d228f3eb04e1
Jonathan Tompson <jonathantompson@gmail.com>
2016-06-16 08:46:04 -0700

    Quick fix to python3 script.

--- compile_python3.4_unix.sh
+++ compile_python3.4_unix.sh
@@ -3,7 +3,7 @@ echo "Compiling python (swig) wrapper..."
 swig -c++ -python -modern -py3 matlabnoise.i
 g++ -fpic -c vec2.cpp vec3.cpp vec4.cpp noise_common.cpp matlabnoise_wrap.cxx \
     -I/usr/local/include \
-    -I/usr/include/python2.7
+    -I/usr/include/python3.4
 g++ -shared vec2.o vec3.o vec4.o noise_common.o matlabnoise_wrap.o \
     -o _matlabnoise.so 
 echo "Add done!"
